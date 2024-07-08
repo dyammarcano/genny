8ef1c8a3e40c2c75ceeb9c1417ec6ca554472b30
Dyam Marcano <dyam.marcano@gmail.com>
2024-07-08 11:23:32 -0300

    first commit

--- fakeit.go
+++ fakeit.go
@@ -1,3 +1,4 @@
 package main
 
 //"github.com/brianvoe/gofakeit/v6"
+// cpf, cnpj, credit card, email, first name, full name, last name, phone number, ssn, username, password, url, uuid, word, sentence, paragraph
